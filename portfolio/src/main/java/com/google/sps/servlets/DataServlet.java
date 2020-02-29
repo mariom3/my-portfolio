5e19a6b7fed5787315e8f22ad6cfcc6303f09ad5
Mario Morales <mmorales@sps-program.com>
2020-02-28 18:41:48 -0600

    Handle comments added view POST form by adding to arraylist

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,9 +15,8 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,34 +26,47 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-    private Map<String, String> comments = new HashMap<String, String>();
+  // IMPROVE: redundently storing display names
+  private List<List<String>> comments = new ArrayList<List<String>>();
 
-    @Override
-    public void init() {
-        comments.put("Mario", "Hi!");
-        comments.put("Mario's imaginary friend", "Hi! What are you going?");
-        comments.put("Mario", "Just learning how to use JSON");
-        comments.entrySet().forEach(entry->{
-            System.out.println(entry.getKey() + ": " + entry.getValue());  
-        });
-    } 
+  @Override
+  public void init() {
+    comments.add(new ArrayList<String>());
+    comments.add(new ArrayList<String>());
+    comments.add(new ArrayList<String>());
+    comments.get(0).add("Mario");
+    comments.get(0).add("Hi!");
+    comments.get(1).add("Mario's imaginary friend");
+    comments.get(1).add("Hi! What are you going?");
+    comments.get(2).add("Mario");
+    comments.get(2).add("Just learning how to use JSON");
+  } 
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String json = convertToJson(comments);
-        System.out.println("Sending string: " + json);
-        response.setContentType("application/json;");
-        response.getWriter().println(json);
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String json = convertToJson(comments);
+    System.out.println("Sending string: " + json);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    comments.add(new ArrayList<String>());
+    comments.get(comments.size()-1).add(request.getParameter("display-name"));
+    comments.get(comments.size()-1).add(request.getParameter("comment"));
+    response.sendRedirect("/aboutme.html");
+  }
+
+  public String convertToJson(List<List<String>> comments) {
+    String json = "{ \"comments\": [";
+    for(List<String> comment : comments){
+      json += "{\"userName\": \"" + comment.get(0)
+        + "\", \"comment\": \"" + comment.get(1) + "\"},";
     }
+    json = json.substring(0, json.length() - 1);
+    json += "]}";
+    return json;
+  }
 
-    public String convertToJson(Map<String, String> comments) {
-        String json = "{ \"comments\": [";
-        for(Entry<String, String> entry : comments.entrySet()){
-            json += "{\"userName\": \"" + entry.getKey()
-                + "\", \"comment\": \"" + entry.getValue() + "\"},";
-        }
-        json = json.substring(0, json.length() - 1);
-        json += "]}";
-        return json;
-    }
 }
