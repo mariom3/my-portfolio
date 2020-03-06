d1cdc11b8a4a0d00bdb1617b45cf48995413c4ae
Mario Morales <mmorales3@hawk.iit.edu>
2020-03-05 19:07:03 -0600

    Display 'add-comment' form only if user is logged in

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,12 +13,11 @@
 // limitations under the License.
 
 
-// TODO: Add logout button
+// TODO: Separate into 'getComments' and 'getLoggedInStatus'
 function getComments() {
   fetch('/data').then(response => response.json()).then((json) => {
     const commentsElement = document.getElementById('comments');
     commentsElement.innerText = '';
-    const loggedInStatus = json.loggedInStatus;
     const comments = json.comments;
     console.log('Received: ' + comments + ", status: " + loggedInStatus);
     for (let i = 0; i < comments.length; i++) {
@@ -36,4 +35,69 @@ function createCommentElement(userName, comment) {
   divElement.innerText = userName + ': ';
   divElement.appendChild(pElement);
   return divElement;
+}
+
+function getLoggedInStatus() {
+  fetch('/data').then(response => response.json()).then((json) => {
+    const loginButton = document.getElementById('login-logout');
+    const loggedIn = json.loggedInStatus;
+    if (loggedIn) {
+      // add 'logged in as __, [logout]'
+      // add form
+      createCommentForm();
+    }else {
+      // login to comment
+      const loginButton = document.createElement('button');
+      loginButton.innerText = 'LOGIN TO ADD A COMMENT';
+      loginButton.setAttribute('class', 'btn btn-outline-dark custom-btn title-font');
+      addCommentElement.appendChild(loginButton);
+    }
+  });
+}
+
+function createCommentForm() {
+  const email = "mmorales@sps-program.com";
+
+  const addCommentElement = document.getElementById('login-logout');
+  addCommentElement.innerText = '';
+
+  const pElement = document.createElement('p');
+  pElement.textContent = "Logged in as: " + email;
+
+  const addCommentForm = document.createElement('form');
+  addCommentForm.setAttribute('action', '/data');
+  addCommentForm.setAttribute('method', 'POST');
+
+  const commentTextArea = document.createElement('textarea');
+  commentTextArea.setAttribute('id', 'comment');
+  commentTextArea.setAttribute('name', 'comment');
+  commentTextArea.setAttribute('rows', '5');
+  commentTextArea.setAttribute('cols', '37');
+  commentTextArea.innerText = 'Add a comment...';
+
+  const logoutButton = document.createElement('button');
+  logoutButton.innerText = 'LOGOUT';
+  logoutButton.setAttribute('class', 'btn btn-outline-dark custom-btn title-font');
+
+  const logoutButtonContainer = document.createElement('div');
+  logoutButtonContainer.innerText = '';
+  logoutButtonContainer.appendChild(logoutButton);
+  logoutButtonContainer.setAttribute('style', 'padding-right: .3em; display: inline;');
+
+  const submitButton = document.createElement('input');
+  submitButton.setAttribute('class', 'btn btn-outline-dark custom-btn title-font');
+  submitButton.setAttribute('type', 'submit');
+  submitButton.setAttribute('value', 'SUBMIT');
+
+  const submitButtonContainer = document.createElement('div');
+  submitButtonContainer.innerText = '';
+  submitButtonContainer.appendChild(submitButton);
+  submitButtonContainer.setAttribute('style', 'padding-right: .3em; display: inline;');
+
+  addCommentForm.appendChild(commentTextArea);
+  addCommentForm.appendChild(document.createElement('br'));
+  addCommentForm.appendChild(logoutButtonContainer);
+  addCommentForm.appendChild(submitButtonContainer);
+  addCommentElement.appendChild(pElement);
+  addCommentElement.appendChild(addCommentForm);
 }
