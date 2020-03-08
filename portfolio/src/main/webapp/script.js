db08fa572a14b6448c62e80512d95011419bf308
Mario Morales <codingmario3@gmail.com>
2020-03-08 08:59:59 -0500

    Remove unnecessary comment

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-// TODO: Separate into 'getComments' and 'getLoggedInStatus'
 function getComments() {
   fetch('/comments').then(response => response.json()).then((json) => {
     const commentsElement = document.getElementById('comments');
